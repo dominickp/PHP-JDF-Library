e33497e5f350db1a5cc2a17936d7469fd92790d2
Dominick Peluso <dominick@p.elu.so>
2014-02-12 21:42:20 -0500

    Cleanup code

--- example.php
+++ example.php
@@ -1,31 +1,18 @@
 <?php
 
-// Include the parameters
-include 'parameters.php';
+include 'parameters.php'; // Include the parameters
+include 'JDF/CreateJDF.php'; // Include the JDF class
 
-// Include the JDF class
-include 'JDF/CreateJDF.php';
-
-// Create the JDF Object
-$JDF = new CreateJDF('MyTestJDF', 'DigitalPrinting');
-
-// Set a comment
-$JDF->setComment("Test comment");
-
-// Set substrate
-$JDF->setMedia("Substrate Name 1");
-
-
-// Input the file
+// Example
 $MyFile = 'example_image.jpg';
-$JDF->setFile($InputLocation.$MyFile);
 
+$JDF = new CreateJDF('MyTestJDF', 'DigitalPrinting'); // Create the JDF Object
+$JDF->setComment("Test comment"); // Set a comment
+$JDF->setMedia("Substrate Name 1"); // Set substrate
+$URL = $JDF->setFile($InputLocation.$MyFile); // Input the file, returns the final file URL
 
-// Call the getXML method
-$created = $JDF->getXML();
+$created = $JDF->getXML(); // Call the getXML method
 
 Header('Content-type: text/xml');
 
 print_r($created);
-
-?>
