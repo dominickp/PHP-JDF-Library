698d14a5f3805a3d6af5942d0eef24eece68f3a1
Dominick Peluso <peluso.dominick@gmail.com>
2014-02-13 22:18:22 -0500

    Reformat code

--- JDF/CreateJDF.php
+++ JDF/CreateJDF.php
@@ -5,42 +5,42 @@ include 'parameters.php';
 class CreateJDF
 {
 
-	public $JDFInitialize;
+    public $JDFInitialize;
 
-	public $ResourcePool;
+    public $ResourcePool;
 
-	public $ResourceLinkPool;
+    public $ResourceLinkPool;
 
-	public $FileDirectory;
+    public $FileDirectory;
 
-	public $Params;
+    public $Params;
 
-	public function __construct($DescriptiveName, $Types)
-	{
-		global $UltimateFileDestination;
-		$this->FileDirectory = $UltimateFileDestination;
+    public function __construct($DescriptiveName, $Types)
+    {
+        global $UltimateFileDestination;
+        $this->FileDirectory = $UltimateFileDestination;
 
-		// These are used to generate the initial XML field attributes
-		$XMLEncoding = '<?xml version="1.0" encoding="UTF-8"?>';
-		$JDFType = 'Combined';
-		$XMLNS = 'http://www.CIP4.org/JDFSchema_1_1';
-		$Id = 'rootNodeId';
-		$Status = 'Waiting';
-		$JobPartId = '000.cdp.797';
+        // These are used to generate the initial XML field attributes
+        $XMLEncoding = '<?xml version="1.0" encoding="UTF-8"?>';
+        $JDFType = 'Combined';
+        $XMLNS = 'http://www.CIP4.org/JDFSchema_1_1';
+        $Id = 'rootNodeId';
+        $Status = 'Waiting';
+        $JobPartId = '000.cdp.797';
 
         // Check for acceptable types
         $AcceptableTypes = array(
             'Binding', 'Cutting', 'DigitalPrinting', 'FinalImaging', 'FinalRIPing', 'Folding', 'Newsprinting', 'PostPress', 'PrePress', 'Printing', 'ProofImaging', 'ProofRIPing', 'PublishingPreparation', 'RIPing', 'WebPrinting'
         );
 
-        if(!in_array($Types, $AcceptableTypes)) throw new Exception("[$Types] is not an acceptable JDF type.");
+        if (!in_array($Types, $AcceptableTypes)) throw new Exception("[$Types] is not an acceptable JDF type.");
 
-		$Version = '1.3';
+        $Version = '1.3';
 
-		// Initialize the JDF
-		$JDFStart = $XMLEncoding.'<JDF Type="'. $JDFType .'" xmlns="'. $XMLNS .'" ID="'. $Id .'" Status="'. $Status .'" JobPartID="'. $JobPartId .'" Version="'. $Version .'" Types="'. $Types .'" DescriptiveName="'. $DescriptiveName .'"></JDF>';
+        // Initialize the JDF
+        $JDFStart = $XMLEncoding . '<JDF Type="' . $JDFType . '" xmlns="' . $XMLNS . '" ID="' . $Id . '" Status="' . $Status . '" JobPartID="' . $JobPartId . '" Version="' . $Version . '" Types="' . $Types . '" DescriptiveName="' . $DescriptiveName . '"></JDF>';
 
-		$this->JDFInitialize = new SimpleXMLElement($JDFStart, LIBXML_NOEMPTYTAG);
+        $this->JDFInitialize = new SimpleXMLElement($JDFStart, LIBXML_NOEMPTYTAG);
 
         // Add some information about where this JDF came from
         $AuditPool = $this->JDFInitialize->addChild('AuditPool');
@@ -49,76 +49,76 @@ class CreateJDF
         $Comments->addAttribute("TimeStamp", date("Y-m-d H:i:s"));
 
         // Add children that we will need regardless
-		$this->setResourcePool();
-		$this->setResourceLinkPool();
-		// Build out the params child
-		$this->setParams($Types);
-		// Build out the component child
-		$this->setComponent();
-	}
+        $this->setResourcePool();
+        $this->setResourceLinkPool();
+        // Build out the params child
+        $this->setParams($Types);
+        // Build out the component child
+        $this->setComponent();
+    }
 
-	public function setResourcePool()
-	{
-		$this->ResourcePool = $this->JDFInitialize->addChild("ResourcePool");
-	}
+    public function setResourcePool()
+    {
+        $this->ResourcePool = $this->JDFInitialize->addChild("ResourcePool");
+    }
 
-	public function setResourceLinkPool()
-	{
-		$this->ResourceLinkPool = $this->JDFInitialize->addChild("ResourceLinkPool");
-	}
+    public function setResourceLinkPool()
+    {
+        $this->ResourceLinkPool = $this->JDFInitialize->addChild("ResourceLinkPool");
+    }
 
-	public function setParams($Types, $Class="Parameter", $ID="DPP001", $Status="Available")
-	{
-		$this->Params = $this->ResourcePool->addChild($Types."Params");
-		$this->Params->addAttribute("Class", $Class);
-		$this->Params->addAttribute("ID", $ID);
-		$this->Params->addAttribute("Status", $Status);
-	}
+    public function setParams($Types, $Class = "Parameter", $ID = "DPP001", $Status = "Available")
+    {
+        $this->Params = $this->ResourcePool->addChild($Types . "Params");
+        $this->Params->addAttribute("Class", $Class);
+        $this->Params->addAttribute("ID", $ID);
+        $this->Params->addAttribute("Status", $Status);
+    }
 
-	public function setComponent($Class="Quantity", $ID="Component", $Status="Unavailable", $ComponentType="FinalProduct")
-	{
-		$Component = $this->ResourcePool->addChild("Component");
-		$Component->addAttribute("Class", $Class);
-		$Component->addAttribute("ID", $ID);
-		$Component->addAttribute("Status", $Status);
-		$Component->addAttribute("ComponentType", $ComponentType);
-	}
+    public function setComponent($Class = "Quantity", $ID = "Component", $Status = "Unavailable", $ComponentType = "FinalProduct")
+    {
+        $Component = $this->ResourcePool->addChild("Component");
+        $Component->addAttribute("Class", $Class);
+        $Component->addAttribute("ID", $ID);
+        $Component->addAttribute("Status", $Status);
+        $Component->addAttribute("ComponentType", $ComponentType);
+    }
 
-	public function setComment($Comment)
+    public function setComment($Comment)
     {
         $Comments = $this->JDFInitialize->addChild('Comment', $Comment);
         $Comments->addAttribute("Name", "GeneralComments");
     }
 
-	public function setMedia($SubstrateName, $MediaID = 'M001', $Status = 'Available')
+    public function setMedia($SubstrateName, $MediaID = 'M001', $Status = 'Available')
     {
         $Media = $this->ResourcePool->addChild("Media");
         $Media->addAttribute("Class", "Consumable");
         $Media->addAttribute("ID", $MediaID);
         $Media->addAttribute("Status", $Status);
         $Media->addAttribute("DescriptiveName", $SubstrateName);
-		// Set the linked Params media reference
-		$MediaRef = $this->Params->addChild("MediaRef");
-		$MediaRef->addAttribute("rRef", $MediaID);
+        // Set the linked Params media reference
+        $MediaRef = $this->Params->addChild("MediaRef");
+        $MediaRef->addAttribute("rRef", $MediaID);
     }
 
-	public function setDevice($IDUsage="QueueDestination", $IDValue="Held", $Class="Implementation", $ID="D001", $Status="Available")
-	{
-		$Device = $this->ResourcePool->addChild("Device");
-		$Device->addAttribute("Class", $Class);
-		$Device->addAttribute("ID", $ID);
-		$Device->addAttribute("Status", $Status);
+    public function setDevice($IDUsage = "QueueDestination", $IDValue = "Held", $Class = "Implementation", $ID = "D001", $Status = "Available")
+    {
+        $Device = $this->ResourcePool->addChild("Device");
+        $Device->addAttribute("Class", $Class);
+        $Device->addAttribute("ID", $ID);
+        $Device->addAttribute("Status", $Status);
 
-		$GeneralID = $Device->addChild("GeneralID");
-		$GeneralID->addAttribute("IDUsage", $IDUsage);
-		$GeneralID->addAttribute("IDValue", $IDValue);
-	}
+        $GeneralID = $Device->addChild("GeneralID");
+        $GeneralID->addAttribute("IDUsage", $IDUsage);
+        $GeneralID->addAttribute("IDValue", $IDValue);
+    }
 
-    public function setLayoutPreparationParams($Sides="OneSidedFront", $ImpositionTemplateURL=null, $ID="LPP001", $Status="Available", $Class="Parameter")
-	{
+    public function setLayoutPreparationParams($Sides = "OneSidedFront", $ImpositionTemplateURL = null, $ID = "LPP001", $Status = "Available", $Class = "Parameter")
+    {
         // Check to make sure the selected Sides variable is an OK JDF value
         $AcceptableTypes = array('OneSidedBackFlipX', 'OneSidedBackFlipY', 'OneSidedFront', 'TwoSidedFlipX', 'TwoSidedFlipY');
-        if(!in_array($Sides, $AcceptableTypes)) throw new Exception("[$Sides] is not an acceptable JDF EnumSides value.");
+        if (!in_array($Sides, $AcceptableTypes)) throw new Exception("[$Sides] is not an acceptable JDF EnumSides value.");
 
         $LayoutPreparationParams = $this->ResourcePool->addChild("LayoutPreparationParams");
         $LayoutPreparationParams->addAttribute("Class", $Class);
@@ -126,49 +126,48 @@ class CreateJDF
         $LayoutPreparationParams->addAttribute("Status", $Status);
         $LayoutPreparationParams->addAttribute("Sides", $Sides);
 
-        if(!empty($ImpositionTemplateURL))
-        {
+        if (!empty($ImpositionTemplateURL)) {
             $ExternalImpositionTemplate = $LayoutPreparationParams->addChild("ExternalImpositionTemplate");
             $FileSpec = $ExternalImpositionTemplate->addChild("FileSpec");
             $FileSpec->addAttribute("URL", $ImpositionTemplateURL);
         }
-	}
+    }
 
-	public function setFile($LocalFile, $RunListID = 'RunList_1', $Status = 'Available')
-	{
-		$RunList = $this->ResourcePool->addChild("RunList");
-		$RunList->addAttribute("ID", $RunListID);
-		$RunList->addAttribute("Status", $Status);
-		$RunList->addAttribute("Class", 'Parameter');
+    public function setFile($LocalFile, $RunListID = 'RunList_1', $Status = 'Available')
+    {
+        $RunList = $this->ResourcePool->addChild("RunList");
+        $RunList->addAttribute("ID", $RunListID);
+        $RunList->addAttribute("Status", $Status);
+        $RunList->addAttribute("Class", 'Parameter');
 
-		$LayoutElement = $RunList->addChild("LayoutElement");
+        $LayoutElement = $RunList->addChild("LayoutElement");
 
-		$FileSpec = $LayoutElement->addChild("FileSpec");
+        $FileSpec = $LayoutElement->addChild("FileSpec");
 
-		// Get MIME type of file passed through
-		if(!file_exists($LocalFile)) throw new Exception("[$LocalFile] cannot be found and therefore the MIME type cannot be determined.");
-		$FileInfo = finfo_open(FILEINFO_MIME_TYPE);
-		$MIMEType = finfo_file($FileInfo, $LocalFile);
-		finfo_close($FileInfo);
-		$FileSpec->addAttribute("MimeType", $MIMEType);
+        // Get MIME type of file passed through
+        if (!file_exists($LocalFile)) throw new Exception("[$LocalFile] cannot be found and therefore the MIME type cannot be determined.");
+        $FileInfo = finfo_open(FILEINFO_MIME_TYPE);
+        $MIMEType = finfo_file($FileInfo, $LocalFile);
+        finfo_close($FileInfo);
+        $FileSpec->addAttribute("MimeType", $MIMEType);
 
-		$URL = $this->FileDirectory.basename($LocalFile);
-		$FileSpec->addAttribute("URL", $URL);
-		// Return the final file destination that the print device will be looking for
-		return $URL;
-	}
+        $URL = $this->FileDirectory . basename($LocalFile);
+        $FileSpec->addAttribute("URL", $URL);
+        // Return the final file destination that the print device will be looking for
+        return $URL;
+    }
 
-	public function setCustomerInfo($BillingCode, $CustomerID)
-	{
-		$CustomerInfo = $this->JDFInitialize->addChild("CustomerInfo");
-		$CustomerInfo->addAttribute("BillingCode", $BillingCode);
-		$CustomerInfo->addAttribute("CustomerID", $CustomerID);
-	}
+    public function setCustomerInfo($BillingCode, $CustomerID)
+    {
+        $CustomerInfo = $this->JDFInitialize->addChild("CustomerInfo");
+        $CustomerInfo->addAttribute("BillingCode", $BillingCode);
+        $CustomerInfo->addAttribute("CustomerID", $CustomerID);
+    }
 
-	public function getXML()
-	{
+    public function getXML()
+    {
         $ReturnXML = $this->JDFInitialize->asXML();
         return $ReturnXML;
-	}
+    }
 
 }
