5d3fa5c5c24b7d9cc0d74eed735e803d7a6518e6
Dominick Peluso <peluso.dominick@gmail.com>
2014-04-28 22:27:44 -0400

    Add some structure

--- Docs/inc/nav.php
+++ Docs/inc/nav.php
@@ -24,12 +24,11 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="<?php echo isActive('index.php') ?>"><a href="index.php">Home</a></li>
-        <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usage <b class="caret"></b></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
+            <li><a href="#">JDF</a></li>
+            <li><a href="#">JMF</a></li>
             <li><a href="#">Something else here</a></li>
             <li class="divider"></li>
             <li><a href="#">Separated link</a></li>
--- Docs/index.php
+++ Docs/index.php
@@ -4,26 +4,42 @@
 ?>
 <div class="row">
     <div class="col-md-8">
-    <h2>Description</h2>
-    <p>
-        The Job Definition Format (JDF) is a specification, based on XML, which is designed to help print jobs flow through a shop. The standard allows one format that many vendors can use to define jobs and also send messages about the status of jobs and the graphic equipment which is producing them. <?php echo $libraryTitle; ?> is a collection of PHP classes made to create and send JDF and JMF. The focus of this library will be toward HP's specific implementation.
-    </p>
-    <p>
-        A practical example of how you might use this library could be in a web-to-print workflow. Jobs could come in from the web and using the JDF class, you could create a JDF to send into your Digital Front End (DFE), specifying how it imposes, what paper it prints on and more. This could allow your job to drop on press in an automated fashion. Another example would utilize the JMF spec where you could send a message to a print device (like a DFE) and it would tell you the status of a job.
-    </p>
-  </div>
-  <div class="col-md-4">
-      <h3>Resources</h3>
-      <h4>HP JDF IDP SDK</h4>
-      <p>
+        <div class="row">
+            <div class="col-md-12">
+
+                <h2>Description</h2>
+                <p>
+                    The Job Definition Format (JDF) is a specification, based on XML, which is designed to help print jobs flow through a shop. The standard allows one format that many vendors can use to define jobs and also send messages about the status of jobs and the graphic equipment which is producing them. <?php echo $libraryTitle; ?> is a collection of PHP classes made to create and send JDF and JMF. The focus of this library will be toward HP's specific implementation.
+                </p>
+                <p>
+                    A practical example of how you might use this library could be in a web-to-print workflow. Jobs could come in from the web and using the JDF class, you could create a JDF to send into your Digital Front End (DFE), specifying how it imposes, what paper it prints on and more. This could allow your job to drop on press in an automated fashion. Another example would utilize the JMF spec where you could send a message to a print device (like a DFE) and it would tell you the status of a job.
+                </p>
+
+            </div>
+            <div class="col-md-12">
+
+                <h2>Requirements</h2>
+                <ul>
+                    <li>PHP 5.3+</li>
+                    <li>cURL PHP module</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+
+        <h3>Resources</h3>
+        <h4>HP JDF IDP SDK</h4>
+        <p>
           HP has an SDK outlining their JDF implementation in detail with several examples. You can download this SDK from <a href="https://spp.austin.hp.com/spp/Public/Sdk/SdkPublicDownload.aspx" target="_blank">HP's website <i class="fa fa-external-link"></i></a>. Just look for the <strong>JDF IDP SDK</strong> in the Indigo Digital Press kit area.
-      </p>
+        </p>
 
-      <h4>CIP4 JDF Specification</h4>
-      <p>
+        <h4>CIP4 JDF Specification</h4>
+        <p>
           The JDF specification can be found at <a href="http://www.cip4.org/documents/jdf_specifications/" target="_blank">CIP4's website <i class="fa fa-external-link"></i></a>. Even though this is focused on HP's implementation, it will remain compliant with the spec.
-      </p>
+        </p>
 
-  </div>
+    </div>
 </div>
 <?php include('inc/footer.php'); ?>
