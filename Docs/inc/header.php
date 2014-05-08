7c39fbda4a15bf373cec05b56542cb686dca94da
Dominick Peluso <peluso.dominick@gmail.com>
2014-05-07 20:52:16 -0400

    Get rid of invalid line

--- Docs/inc/header.php
+++ Docs/inc/header.php
@@ -7,7 +7,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="favicon.ico" rel="icon" type="image/x-icon"/>
     <title><?php echo $libraryTitle . ': ' . $pageTitle; ?></title>
