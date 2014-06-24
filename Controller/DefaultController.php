1c7d83d7de5ffe16f8ffcf4258f1ee8ee8fafc7a
Dominick Peluso <peluso.dominick@gmail.com>
2014-06-24 12:17:47 -0400

    Start with skeleton

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DominickPeluso\JdfLibraryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('JdfLibraryBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ JdfLibraryBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DominickPeluso\JdfLibraryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JdfLibraryBundle extends Bundle
+{
+	public function getName(){
+	
+		return 'JdfLibraryBundle';
+		
+	}
+}
