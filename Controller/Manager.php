19f7d0aaca4045e4e611d72b66d62f2202e7ad95
Dominick Peluso <peluso.dominick@gmail.com>
2014-07-11 12:56:11 -0400

    Multi cURL test

--- Controller/Manager.php
+++ Controller/Manager.php
@@ -7,16 +7,31 @@ class Manager
 
     protected $SimpleXMLObject;
 
-    public function load($SimpleXMLObject){
+    protected $multiSimplyXMLObjects;
+
+    public function load($SimpleXMLObject)
+    {
 
         $this->SimpleXMLObject = $SimpleXMLObject;
 
     }
 
-    public function postXML($IDP_Worker){
+    public function loadMulti($array)
+    {
+        $this->multiSimplyXMLObjects = $array;
+    }
 
+    public function checkCurlInstalled()
+    {
         // Make sure cURL is enabled
         if(!function_exists('curl_version')) throw new \Exception("cURL must be installed for sendJMF() to work.");
+
+        return true;
+    }
+
+    public function postXML($IDP_Worker)
+    {
+        $this->checkCurlInstalled();
 
         $ch = curl_init($IDP_Worker);
 
@@ -31,6 +46,64 @@ class Manager
         curl_close($ch);
 
         return $result;
+
+    }
+
+    public function multiPostXML($IDP_Worker)
+    {
+        $this->checkCurlInstalled();
+
+        $mh = curl_multi_init();
+
+        $curlHandles = array();
+
+        foreach($this->multiSimplyXMLObjects as $key => $SimplyXMLObject)
+        {
+            $ch = curl_init($IDP_Worker);
+
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $SimplyXMLObject->asXML());
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-type: application/vnd.cip4-jmf+xml'
+            ));
+
+            $curlHandles[$key] =  $ch;
+            curl_multi_add_handle($mh,$ch);
+
+        }
+
+        $active = null;
+        //execute the handles
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        }
+        while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+        while ($active && $mrc == CURLM_OK) {
+            if (curl_multi_select($mh) != -1) {
+                do {
+                    $mrc = curl_multi_exec($mh, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
+
+        $returnArray = array();
+
+        /* This is the relevant bit */
+        // iterate through the handles and get your content
+        foreach ($curlHandles as $key => $ch) {
+            $response = curl_multi_getcontent($ch); // get the content
+            // do what you want with the HTML
+            curl_multi_remove_handle($mh, $ch); // remove the handle (assuming  you are done with it);
+
+            $returnArray[$key] = $response;
+        }
+        /* End of the relevant bit */
+
+        curl_multi_close($mh); // close the curl multi handler
+
+        return $returnArray;
 
     }
 
